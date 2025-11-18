878b9b819c466a6732cf6502a723f735810d2fa4
Ajay Ambule <ajayambule2003@gmail.com>
2025-11-18 09:16:53 +0530

    ServletJSPStudent Application

---
+++ ServletJSpStudentApp/src/main/java/com/controller/StudentDeleteServlet.java
@@ -0,0 +1,46 @@
+package com.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dao.StudentDAO;
+import com.dao.StudentDAOImpl;
+
+@WebServlet("/StudentDeleteServlet")
+public class StudentDeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public StudentDeleteServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		int id = Integer.parseInt(request.getParameter("id"));
+
+		StudentDAO dao = new StudentDAOImpl();
+
+		int count = dao.deleteStudent(id);
+
+		if (count > 0) {
+			out.print("<h1> Student Delete </h1>");
+			RequestDispatcher rd = request.getRequestDispatcher("display.jsp");
+			rd.include(request, response);
+
+		} else
+			out.print("<h1> FAIL </h1>");
+
+	}
+
+}
